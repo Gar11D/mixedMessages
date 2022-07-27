0b4dabdd25c585cee8a16e647de231ed72f10fc3
Harry Dobbs <hrdobbs@outlook.com>
2022-07-27 19:10:15 +0100

    Improve formatting

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-const subject = ['Dogs', 'Cats', 'People', 'Wizards', 'Fairies', 'Mice', 'Rats', 'Ghosts', 'Goblins', 'Zebras'];
-const verb = ['eat', 'drink', 'desire', 'prefer', 'design', 'calculate', 'paint', 'destroy', 'manipulate', 'praise'];
-const object = ['circuits', 'numbers', 'lettuce', 'medicine', 'facts', 'lamps', 'nothing', 'water', 'fountains', 'schools'];
+// Define words
+const subject = [
+    'Dogs', 'Cats', 'People', 'Wizards', 'Fairies', 'Mice',
+    'Rats', 'Ghosts', 'Goblins', 'Zebras' ];
 
-const randomSubject = subject[Math.floor (Math.random() *10)]
-const randomVerb = verb[Math.floor (Math.random() *10)]
-const randomObject = object[Math.floor (Math.random() *10)]
+const verb = [
+    'eat', 'drink', 'desire', 'prefer', 'design', 'calculate',
+    'paint', 'destroy', 'manipulate', 'praise' ];
 
+const object = ['circuits', 'numbers', 'lettuce', 'medicine', 'facts', 'lamps',
+    'nothing', 'water', 'fountains', 'schools' ];
+
+// Pick words
+const randomSubject = subject[ Math.floor(Math.random() *10) ]
+const randomVerb = verb[ Math.floor(Math.random() *10) ]
+const randomObject = object[ Math.floor(Math.random() *10) ]
+
+// Function to generate a random sentence
 const generateRandomSentence = () => {
     return `${randomSubject} ${randomVerb} ${randomObject}.`
 }
